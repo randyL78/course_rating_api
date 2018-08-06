818813249ccd04d17410cddbc6735d26d7b781d8
Randy's Mac <randydavidl78@gmail.com>
2018-08-05 22:09:41 -0400

    Hash user passwords before storing in DB

--- models/user.js
+++ models/user.js
@@ -1,6 +1,13 @@
+/* User document schema for MongoDB/Mongoose */
+
+// load vendor modules
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// other globals
 const Schema = mongoose.Schema;
 
+// create User schema
 const UserSchema = new Schema({
   _id : {
     type    : Schema.ObjectId,
@@ -19,6 +26,20 @@ const UserSchema = new Schema({
     type    : String,
     required: true
   }
+});
+
+// hash password before saving to DB
+// must use traditional function syntax here so that `this` is bound to current user
+UserSchema.pre('save', function (next) {
+  const user = this;
+
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) {
+      next(err);
+    }
+    user.password = hash;
+    next();
+  })
 });
 
 const User = mongoose.model('User', UserSchema);
